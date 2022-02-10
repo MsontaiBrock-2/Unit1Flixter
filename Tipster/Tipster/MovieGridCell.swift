66dc72898df61009f6de9a605a71dbd7b941e687
MsontaiBrock-2 <69806383+MsontaiBrock-2@users.noreply.github.com>
2022-02-09 23:26:36 -0600

    Added Unit 2: Flixter

---
+++ Tipster/Tipster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Tipster
+//
+//  Created by Msontai Brock on 2/9/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+   
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
